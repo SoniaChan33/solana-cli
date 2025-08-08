07900d9e4250099394569724da9441984175e9f3
sonia33 <soniachan33@126.com>
2025-08-08 15:49:41 +0800

    feat:cli:test_mint_to_ata

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use solana_sdk::signature::Signer;
 use solana_sdk::signer::keypair::read_keypair_file;
 use solana_sdk::sysvar;
 use solana_sdk::transaction::Transaction;
+use spl_associated_token_account::get_associated_token_address;
 fn main() {}
 
 #[derive(BorshSerialize, BorshDeserialize)]
@@ -28,13 +29,22 @@ fn test_fn() {
     let mint_account = Keypair::new();
     println!("Mint Account: {}", mint_account.pubkey().to_string());
 
-    create_token(
+    _ = create_token(
         &rpc_client,
         &program_id,
         &payer,
         &mint_account,
         &payer.pubkey(),
         6,
+    );
+
+    _ = mint(
+        &rpc_client,
+        &program_id,
+        &payer,
+        &mint_account,
+        &payer.pubkey(),
+        100_000_000,
     );
 }
 
@@ -87,6 +97,70 @@ fn create_token(
 
     println!(
         "Token created successfully with mint account: {}",
+        mint_account.pubkey()
+    );
+    Ok(())
+}
+
+/**
+ * 铸币
+ */
+fn mint(
+    rpc_client: &RpcClient,
+    program_id: &Pubkey,
+    payer: &Keypair,
+    mint_account: &Keypair,
+    mint_authority: &Pubkey,
+    amount: u64,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // ata 账户
+    let ata_account = get_associated_token_address(&payer.pubkey(), &mint_account.pubkey());
+    println!("ATA Account: {}", ata_account.to_string());
+    // 这里可以添加铸币的逻辑
+    // 例如，调用合约的 mint
+    let instruction_data: Vec<u8> = borsh::to_vec(&TokenInstruction::Mint { amount }).unwrap();
+
+    // 传这六个项目
+    //     let mint_account = next_account_info(account_iter)?;
+    //     let associated_token_account = next_account_info(account_iter)?;
+    //     let rent_sysvar = next_account_info(account_iter)?;
+    //     let payer = next_account_info(account_iter)?;
+    //     let system_program = next_account_info(account_iter)?;
+    //     let token_program = next_account_info(account_iter)?;
+    //     let associated_token_program = next_account_info(account_iter)?;
+
+    let accounts: Vec<AccountMeta> = vec![
+        AccountMeta::new(mint_account.pubkey(), true),
+        AccountMeta::new_readonly(*mint_authority, false),
+        AccountMeta::new_readonly(payer.pubkey(), true),
+        AccountMeta::new_readonly(solana_sdk::system_program::id(), false), // 可传可不传
+        AccountMeta::new_readonly(spl_token::id(), false),
+        AccountMeta::new_readonly(spl_associated_token_account::id(), false),
+    ];
+
+    // 创建指令
+    let token_instruction = Instruction {
+        program_id: *program_id,
+        accounts,
+        data: instruction_data,
+    };
+
+    // 发送交易
+    // 获取最新区块哈希
+    let latest_blockhash = rpc_client.get_latest_blockhash()?;
+    // 创建交易
+    let tx = Transaction::new_signed_with_payer(
+        &[token_instruction],
+        Some(&payer.pubkey()),
+        &[payer, mint_account],
+        latest_blockhash,
+    );
+    let r = rpc_client.send_and_confirm_transaction(&tx)?;
+
+    println!("Transaction result: {:?}", r);
+
+    println!(
+        "Token minted successfully to account: {}",
         mint_account.pubkey()
     );
     Ok(())
